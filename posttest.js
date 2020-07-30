6c0251eefea7802fba98fce4c654dbca77099750
KartikayB <kartikay.bansal2014@gmail.com>
2020-07-30 22:51:56 +0530

    Changes In Pre And Post Test

--- posttest.js
+++ posttest.js
@@ -1,34 +1,34 @@
 function result() {
     if (document.getElementById('q13').checked) {
-        document.getElementById('q1c').innerHTML = "correct";
+        document.getElementById('q1c').innerHTML = "Correct";
     } else {
-        document.getElementById('q1w').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q1w').innerHTML = "Wrong Answer, the correct option is B ";
 
     }
 
     if (document.getElementById('q22').checked) {
-        document.getElementById('q2c').innerHTML = "correct";
+        document.getElementById('q2c').innerHTML = "Correct";
     } else {
-        document.getElementById('q2w').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q2w').innerHTML = "Wrong Answer, the correct option is B ";
 
     }
 
     if (document.getElementById('q33').checked) {
-        document.getElementById('q3c').innerHTML = "correct";
+        document.getElementById('q3c').innerHTML = "Correct";
     } else {
-        document.getElementById('q3w').innerHTML = "wrong ans, the correct option is C ";
+        document.getElementById('q3w').innerHTML = "Wrong Answer, the correct option is C ";
 
     }
     if (document.getElementById('q44').checked) {
-        document.getElementById('q4c').innerHTML = "correct";
+        document.getElementById('q4c').innerHTML = "Correct";
     } else {
-        document.getElementById('q4w').innerHTML = "wrong ans, the correct option is D ";
+        document.getElementById('q4w').innerHTML = "Wrong Answer, the correct option is D ";
 
     }
     if (document.getElementById('q51').checked) {
-        document.getElementById('q5c').innerHTML = "correct";
+        document.getElementById('q5c').innerHTML = "Correct";
     } else {
-        document.getElementById('q5w').innerHTML = "wrong ans, the correct option is A ";
+        document.getElementById('q5w').innerHTML = "Wrong Answer, the correct option is A ";
 
     }
 }
--- pretest.js
+++ pretest.js
@@ -1,42 +1,72 @@
 function result1() {
     if (document.getElementById('q1').checked) {
-        document.getElementById('q1p').innerHTML = "correct";
+        document.getElementById('q1p').innerHTML = "Correct";
         document.getElementById('q1p').style.color="green"
+        document.getElementById('q1p').style.fontSize="18px"
+        document.getElementById('q1p').style.fontWeight="bold"
+
     } else {
-        document.getElementById('q1p').innerHTML = "wrong ans, the correct option is A ";
+        document.getElementById('q1p').innerHTML = "Wrong Answer, the correct option is A ";
         document.getElementById('q1p').style.color="red"
+        document.getElementById('q1p').style.fontSize="18px"
+        document.getElementById('q1p').style.fontWeight="bold"
+
 
     }
 
     if (document.getElementById('q8').checked) {
-        document.getElementById('q2p').innerHTML = "correct";
+        document.getElementById('q2p').innerHTML = "Correct";
         document.getElementById('q2p').style.color="green"
+        document.getElementById('q2p').style.fontSize="18px"
+        document.getElementById('q2p').style.fontWeight="bold"
+
         
     } else {
-        document.getElementById('q2p').innerHTML = "wrong ans, the correct option is D ";
+        document.getElementById('q2p').innerHTML = "Wrong Answer, the correct option is D ";
         document.getElementById('q2p').style.color="red"
+        document.getElementById('q2p').style.fontSize="18px"
+        document.getElementById('q2p').style.fontWeight="bold"
+
     }
 
     if (document.getElementById('q10').checked) {
-        document.getElementById('q3p').innerHTML = "correct";
+        document.getElementById('q3p').innerHTML = "Correct";
         document.getElementById('q3p').style.color="green"
+        document.getElementById('q3p').style.fontSize="18px"
+        document.getElementById('q3p').style.fontWeight="bold"
+
     } else {
-        document.getElementById('q3p').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q3p').innerHTML = "Wrong Answer, the correct option is B ";
         document.getElementById('q3p').style.color="red"
+        document.getElementById('q3p').style.fontSize="18px"
+        document.getElementById('q3p').style.fontWeight="bold"
+
     }
     if (document.getElementById('q14').checked) {
-        document.getElementById('q4p').innerHTML = "correct";
+        document.getElementById('q4p').innerHTML = "Correct";
         document.getElementById('q4p').style.color="green"
+        document.getElementById('q4p').style.fontSize="18px"
+        document.getElementById('q4p').style.fontWeight="bold"
+
     } else {
-        document.getElementById('q4p').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q4p').innerHTML = "Wrong Answer, the correct option is B ";
         document.getElementById('q4p').style.color="red"
+        document.getElementById('q4p').style.fontSize="18px"
+        document.getElementById('q4p').style.fontWeight="bold"
+
     }
     if (document.getElementById('q18').checked) {
-        document.getElementById('q5p').innerHTML = "correct";
+        document.getElementById('q5p').innerHTML = "Correct";
         document.getElementById('q5p').style.color="green"
+        document.getElementById('q5p').style.fontSize="18px"
+        document.getElementById('q5p').style.fontWeight="bold"
+
     } else {
-        document.getElementById('q5p').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q5p').innerHTML = "Wrong Answer, the correct option is B ";
         document.getElementById('q5p').style.color="red"
+        document.getElementById('q5p').style.fontSize="18px"
+        document.getElementById('q5p').style.fontWeight="bold"
+
     }
 }
 
